5ed013b7314bba4ddcb3106f2f97b2a4dbfc77e8
wangbin <253498229@qq.com>
2018-04-14 19:29:50 +0800

    加入docker

--- sql/test.sql
+++ sql/test.sql
@@ -1,35 +1,27 @@
-select
-  id,
-  access_token_validity_seconds,
-  additional_information,
-  authorities,
-  authorized_grant_types,
-  auto_approve_scope,
-  client_id,
-  client_secret,
-  refresh_token_validity_seconds,
-  registered_redirect_uri,
-  resource_ids,
-  scope
-from test.t_oauth_client_details;
-
-insert into t_oauth_client_details
+insert ignore into t_oauth_client_details
 (
   id,
   client_id,
   client_secret,
   access_token_validity_seconds,
   refresh_token_validity_seconds,
-  authorized_grant_types
-
+  authorized_grant_types,
+  scope
 )
 values
   (
-    '1',
+    1,
     'client',
     'secret',
     7200,
     604800,
-    'authorization_code,refresh_token,password'
+    'authorization_code,refresh_token,password',
+    'app'
+  );
 
-  )
+insert ignore into t_user (id, enabled, password, username)
+values (1, true, '$2a$10$fgZ5WShCP0C6j7KX6k4MpuYTGUqtRgJfORZXCCw9JnFjkVYYqczky', 'user');
+
+insert ignore into t_role (id, name) values (1, 'USER');
+
+insert ignore into t_user_role (user_id, role_id) values (1, 1);
